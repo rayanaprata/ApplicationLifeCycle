33dd22c653cf51ab6a72ff9f8cbcfb7913e0ce11
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-11-05 10:20:17 -0300

    Initial Commit

---
+++ ApplicationLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApplicationLifeCycle
+//
+//  Created by Rayana Prata Neves on 05/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
